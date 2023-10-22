1f0c672ffddf35fd7ab85dcf3a994be4cc779a05
firda <yuliantifirda966@gmail.com>
2023-10-22 23:25:10 +0700

    new

---
+++ getpost/js/template/template.js
@@ -0,0 +1,15 @@
+export let URLGeoJson = "https://us-central1-gis3-401509.cloudfunctions.net/punyafirda";
+export let tableTag = "tr";
+export let tableRowClass = "content is-small";
+export let tableTemplate = `
+<td>#NAME#</td>
+<td >#KORDINAT#</td>
+<td>#TYPE#</td>
+`;
+export const clickpopup = `
+Long : #LONG#<br>
+Lat  : #LAT#<br>
+X   : #X#<br>
+Y   : #Y#<br>
+HDMS : #HDMS#<br>
+`;
